58edbae8bb2c80a748b4250813099d5a5615301c
3dSpace116 <mirskam@gmail.com>
2024-03-04 19:12:58 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         //Задача 1
+
         byte mouse=2;
         short dog=10;
         int horse=15;
@@ -10,7 +12,9 @@ public class Main {
         System.out.println("Значение переменной "+ "dog"+ " с типом " + "short"+ " равно "+ dog);
         System.out.println("Значение переменной "+ "horse"+ " с типом " + "int"+ " равно "+ horse);
         System.out.println("Значение переменной "+ "cat"+ " с типом " + "long"+ " равно "+ cat);
+
         //Задача 2
+
         float a=27.12F;
         long big= 987678965549L;
         float c=2.786F;
@@ -18,14 +22,18 @@ public class Main {
         short f=-159;
         short g=27897;
         byte h=67;
+
         //Задача 3
+
         byte lyudmilaPavlovna =23;
         byte annaSegeevna=27;
         byte ekaterinaAndreevna=30;
         short totalList =480;
         short  listCount= (short) (totalList/(lyudmilaPavlovna+ekaterinaAndreevna+annaSegeevna));
         System.out.println("На каждого ученика рассчитано "+listCount+" листов бумаги");
+
         //Задача 4
+
         byte proizvoditelnost=16/2;
         short  dvadsatMinut=(short) (20*proizvoditelnost);
         short sutkiVminutah= (short) (1440*proizvoditelnost);
@@ -36,7 +44,9 @@ public class Main {
         System.out.println("За "+"сутки" +" машина произвела "+sutkiVminutah+" штук бутылок");
         System.out.println("За "+"три дня" +" машина произвела "+ triDnyaVminutah+" штук бутылок");
         System.out.println("За "+"месяц" +" машина произвела "+mesyatsVminutah+" штук бутылок");
+
         //Задача 5
+
         byte totalJar=120;
         byte whitePerClass=2;
         byte brownPerClass=4;
@@ -44,7 +54,9 @@ public class Main {
         byte whiteCount= (byte) (classCount*whitePerClass);
         byte brownCount= (byte) (classCount*brownPerClass);
         System.out.println("В школе, где " +classCount+ " классов, нужно "+whiteCount+" банок белой краски и "+brownCount+" банок коричневой краски");
+
         //Задача 6
+
         byte bananaCount=5;
         byte babanaWeight=80;
         byte milkCount= 2;
@@ -56,7 +68,9 @@ public class Main {
         int totalWeight=bananaCount*babanaWeight+milkCount*milkWeight+iceCreamCount*iceCreamWeight+eggCount*eggWeight;
         float weightKg=totalWeight/1000f;
         System.out.println("Общий вес такого спортзавтрака составляет "+weightKg+" кг");
+
         //Задача 7
+
         short totalLoss=7000;
         short slowLoss=250;
         short fastLoss=500;
@@ -67,7 +81,9 @@ public class Main {
         System.out.println("Если терять по "+slowLoss+"грамм в день, то на похудение уйдет "+daysIfSlow+" дней");
         System.out.println("Если терять по "+fastLoss+"грамм в день, то на похудение уйдет "+daysIfFast+" дней");
         System.out.println("Если терять по "+averageLoss+"грамм в день, то на похудение уйдет "+daysAverage+" дней");
+
         //Задача 8
+
         int mashaPayment=67760;
         int denisPayment=83690;
         int kristinaPayment=76230;
